fbb6510a640e96826f6477e3e8b40d1e39316d75
unknown <johnpatrickdfelicia.com>
2024-09-30 12:01:34 +0800

    Call To Action Added. h2 title, h3 sub title, p description, button reservation, header image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav'
+import CallToAction from './components/CallToAction'
 
 function App() {
   return (
     <>
     <Nav />
+    <CallToAction />
     </>
   );
 }
