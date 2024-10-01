d43339a7978a7b5cc6ae96190d6f321289678190
unknown <johnpatrickdfelicia.com>
2024-10-01 17:58:33 +0800

    Reservation Functionality Fixed. Time is now showing. Added a default 'select an option' value to occasion

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -40,12 +40,13 @@ const BookingForm = (props) => {
 
                         <div>
                             <label htmlFor='book-guests'>Number of Guests:</label>
-                            <input id='book-guests' min='1' value={guests} onChange={(e) => setGuests(e.target.value)} />
+                            <input id='book-guests' min='1' value={guests} onChange={(e) => {setGuests(e.target.value)}} type={'number'} placeholder={0} max={10} required />
                         </div>
 
                         <div>
                             <label htmlFor='book-occasion'>Occasion:</label>
                             <select id='book-occasion' key={occasion} value={occasion} onChange={e => setOccasion(e.target.value)}>
+                                <option value="">Select an Option</option>
                                 <option>Birthday</option>
                                 <option>Anniversary</option>
                             </select>
